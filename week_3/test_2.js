8a13039bbb4b714e0d15514e61a100e2a351a7a7
Chisom <chisom@Chisoms-MacBook-Air.local>
2022-10-22 15:31:38 +0100

    all

--- week_3/test_2.js
+++ week_3/test_2.js
@@ -1,6 +1,26 @@
-/*let base = prompt("enter base:");
-let height = prompt("enter height:");
+/*let base = prompt("enter base:" );
+let height = prompt("enter height: ");
 let areOfTriangle = console.log('the area of a triangle is' 0.5 * ${base} * ${height})
 */
-let number = prompt('Enter number', 'number goes here')
-console.log(number)
+//exercise 15
+let Time = new Date()
+let Year = Time.getFullYear()
+let Month = String(Time.getMonth() + 1).padStart(2, '0');
+let Day = String(Time.getDay()).padStart(2, '0')
+let hourly = String(Time.getHours()).padStart(2, '0')
+let minutely = String(Time.getMinutes()).padStart(2, '0')
+console.log(Year + '-' +Month + '-' +Day + ' ' +hourly + ':' + minutely)
+
+//exercise 15 ii//
+console.log(Day + '-' +Month + '-' +Year + ' ' +hourly + ':' + minutely)
+//exercise 15 iii//
+console.log(Year + '/' +Month + '/' +Day + ' ' +hourly + ':' + minutely)
+
+//level 3
+let humanTime = new Date()
+let humanYear = humanTime.getFullYear()
+let humanMonth = String(humanTime.getMonth() + 1).padStart(2, '0');
+let humanDay = String(humanTime.getDay()).padStart(2, '0')
+let hour = String(humanTime.getHours()).padStart(2, '0')
+let minute = String(humanTime.getMinutes()).padStart(2, '0')
+console.log(humanYear + '-' +humanMonth + '-' +humanDay + ' ' +hour + ':' + minute) 
