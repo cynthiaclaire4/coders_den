f16393f051fee6d67b79fc2abff6bff664cdcccd
Chisom <chisom@Chisoms-MacBook-Air.local>
2022-10-27 18:06:33 +0100

    new

---
+++ week_4/exercise_1.js
@@ -0,0 +1,31 @@
+/*
+let age = prompt('Enter your age:')
+
+if (age >= 18) {
+    console.log('You are old enough to drive')
+} else (age == 15) 
+    console.log('You are left with 3 years')
+
+// exercise 2
+let yourAge = prompt('Enter your age:')
+let myAge = 25
+if (yourAge > myAge) {
+    console.log('you are 5 years older than me')
+}
+*/
+//exercise 3
+let a = 4
+let b = 3
+if (a > b) {
+    console.log('a is greater than b')
+} else
+console.log('a is less than b')
+
+a > b ? console.log('a is greater than b') : console.log('a is less than b')
+
+//exercise 4
+let num = prompt('Enter a number:')
+if (num % 2 == 0) {
+    console.log(`${num} is an even number`)
+} else 
+console.log(`${num} is an odd number`)
---
+++ week_4/exercise_2.js
@@ -0,0 +1,31 @@
+//exercise 1
+/*let grade = 60
+switch (grade) {
+    case grade >= 80:
+        console.log('A');
+        break;
+    case grade == 70 && grade <= 89:
+        console.log('B')
+        break;
+    case grade == 60 && grade <= 69:
+        console.log('C')
+        break;
+    case grade == 50 && grade <= 59:
+        console.log('D')
+        break;
+    case grade == 0 && grade <= 49:
+        console.log('F')
+        break;
+}
+*/
+let grade = 50
+if (grade >= 80) {
+    console.log('A')
+} else if (grade >= 79 ) {
+    console.log('B')
+} else if (grade <= 69) {
+    console.log('C')
+} else if (grade <= 59) {
+    console.log('D')
+} else (grade <= 49) 
+    console.log('F')
