87331eba74ef332c39c8dc1a0953b8d40a9a62dd
Chisom <chisom@Chisoms-MacBook-Air.local>
2022-10-28 18:55:01 +0100

    all

--- week_4/exercise_2.js
+++ week_4/exercise_2.js
@@ -1,31 +1,82 @@
 //exercise 1
-/*let grade = 60
-switch (grade) {
-    case grade >= 80:
-        console.log('A');
+
+let grades = 77
+switch (true) { 
+    case grades >= 80 :
+        console.log('A')
         break;
-    case grade == 70 && grade <= 89:
+    case grades >= 70 && grades < 80 :
         console.log('B')
         break;
-    case grade == 60 && grade <= 69:
+    case grades <= 69  && grades > 59 :
         console.log('C')
         break;
-    case grade == 50 && grade <= 59:
+    case grades <= 50 && grades > 49 :
         console.log('D')
         break;
-    case grade == 0 && grade <= 49:
+    case grades == 0 || grades <= 49 :
         console.log('F')
         break;
+    default:
+        console.log('you didnt write exam')
+        break;
 }
-*/
-let grade = 50
+
+let grade = 55
 if (grade >= 80) {
     console.log('A')
-} else if (grade >= 79 ) {
+} else if (grade >= 70 && grade < 80) {
     console.log('B')
-} else if (grade <= 69) {
+} else if (grade <= 69 && grade > 59) {
     console.log('C')
-} else if (grade <= 59) {
+} else if (grade <= 59 && grade > 49) {
     console.log('D')
-} else (grade <= 49) 
+} else if (grade <= 49) 
     console.log('F')
+
+// exercise 2
+let season = prompt('Enter the month:')
+switch (season) {
+    case 'September' :
+    case 'October' :
+    case 'November' :
+        console.log('the season is Autumn')
+        break;
+    case 'December' :
+    case 'January' :
+    case 'February' :
+        console.log('season is winter')
+        break;
+    case 'March':
+    case 'April' : 
+    case 'May' :
+        console.log('the season is spring')
+        break;
+    case 'June':
+    case 'July': 
+    case 'August' :
+        console.log('the season is summer')
+        break;
+    default:
+        console.log('this not a month')
+        break;
+}
+
+//exercise 3
+let day = prompt('What is the day today?')
+switch (day) {
+    case 'saturday' :
+    case 'sunday' :
+        console.log(day + ' is a weekend')
+        break;
+    case 'Monday' :
+    case 'Tuesday' :
+    case 'Wenesday' :
+    case 'Thursday' :
+    case 'Friday' :
+        console.log(day + ' is a working day')
+        break;
+    default:
+        console.log('this is not a day')
+        break;
+}
