3ad91686d8477061ce7f5fa474f26e773f0a0a4b
Chisom <chisom@Chisoms-MacBook-Air.local>
2022-10-28 20:42:53 +0100

    all

---
+++ week_5/level_1.js
@@ -0,0 +1,33 @@
+//Declare an empty array;
+const arr = []
+
+//Declare an array with more than 5 number of elements
+const arr1 = ['me', 45, true,  {country: 'nigeria'}, ['beans', 'rice']]
+
+//Find the length of your array
+console.log(arr1.length)
+
+//Get the first item, the middle item and the last item of the array
+console.log(arr1[0])
+
+//the last item
+let lastIndex = arr1.length - 1
+let lastItem = arr1[lastIndex]
+
+console.log(lastItem)
+
+//middle item
+console.log(arr1[2])
+
+//Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+let mixedDataType = ['me', false, 1, ['us, 3'], 'a', ['a', [2, 3]]]
+
+//declare an array var name it company
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+console.log(itCompanies)
+console.log(itCompanies.length)
+//lastComp = itCompanies.length - 1
+console.log(itCompanies[0])
+console.log(itCompanies.splice(1, last -1))
+console.log(itCompanies.lastComp)
+console.log(lastComp)
